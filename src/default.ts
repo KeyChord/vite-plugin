d2e4e2ac2ec5944adc8800345fa96ef96585f8b0
Leon Si <leon@leonsilicon.com>
2026-03-27 14:05:08 -0400

    use src/ as the default

--- src/default.ts
+++ src/default.ts
@@ -2,14 +2,16 @@ import type { Plugin } from 'vite-plus';
 import fs from 'node:fs';
 import path from 'node:path'
 
+const DEFAULT_INPUT_FILES_ROOT_DIRNAME = 'src';
+
 export default function keychord() {
   return [
     {
       name: 'keychord',
       config(config) {
         const root = config.root ?? process.cwd();
-        const srcDirpath = path.join(root, 'src');
-        const input = fs.readdirSync(srcDirpath).filter(file => file.endsWith('.ts')).map(file => path.join('src', file));
+        const srcDirpath = path.join(root, DEFAULT_INPUT_FILES_ROOT_DIRNAME);
+        const input = fs.readdirSync(srcDirpath).filter(file => file.endsWith('.ts')).map(file => path.join(DEFAULT_INPUT_FILES_ROOT_DIRNAME, file));
 
         return {
           build: {
