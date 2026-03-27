2d5865bff1cb2081b9b77105dee62bbea8dc7ece
Leon Si <leon@leonsilicon.com>
2026-03-27 13:57:33 -0400

    updates

--- src/default.ts
+++ src/default.ts
@@ -1,14 +1,39 @@
 import type { Plugin } from 'vite-plus';
+import fs from 'node:fs';
+import path from 'node:path'
 
 export default function keychord() {
-  return {
-    name: 'keychord',
-    config(this: unknown) {
-      return {
-        build: {
-          outDir: '@/'
+  return [
+    {
+      name: 'keychord',
+      config(config) {
+        const root = config.root ?? process.cwd();
+        const srcDirpath = path.join(root, 'src');
+        const input = fs.readdirSync(srcDirpath).filter(file => file.endsWith('.ts')).map(file => path.join('src', file));
+
+        return {
+          build: {
+            // Since we target a "server" runtime (LLRT)
+            ssr: true,
+
+            // The standard for importing from chords.toml
+            outDir: 'exports',
+
+            rolldownOptions: {
+              input,
+
+              // We want to produce self-contained files
+              external: [],
+              output: {
+                codeSplitting: false,
+              }
+            }
+          },
+          ssr: {
+            noExternal: true,
+          }
         }
       }
-    }
-  } as const satisfies Plugin;
+    } satisfies Plugin
+  ]
 }
